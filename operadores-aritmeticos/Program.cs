61633e56e124ab57b8d9a2a5de4605be96fabe5d
erica-as <erica.asnt@hotmail.com>
2022-11-28 19:02:18 -0300

    operadores aritmeticos

---
+++ operadores-aritmeticos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace operadores_aritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
