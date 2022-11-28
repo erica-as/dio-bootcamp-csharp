3bbb44a294341c2166fee2dcd5c46f19bdb04bc2
erica-as <erica.asnt@hotmail.com>
2022-11-28 07:23:07 -0300

    tipos de operadores

---
+++ tipos-de-operadores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tipos_de_operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
