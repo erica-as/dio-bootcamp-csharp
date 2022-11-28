c1532219c6f945078b6f60a2c4fdaa82cbe21f50
erica-as <erica.asnt@hotmail.com>
2022-11-28 18:16:12 -0300

    operadores codicionais

--- tipos-de-operadores/operadores/OperadoresCondicionais.cs
+++ tipos-de-operadores/operadores/OperadoresCondicionais.cs
@@ -7,6 +7,52 @@ namespace tipos_de_operadores.operadores
 {
     public class OperadoresCondicionais
     {
-        
+        //breakpoint + f5 para iniciar + f10 para continuar
+        int quantidadeEstoque = 4;
+        int quantidadeCompra = 5;
+
+        public void IfAninhado() 
+        {
+            if(quantidadeEstoque >= quantidadeCompra)
+            {
+                Console.WriteLine("Venda realizada.");
+            }
+            else if(quantidadeCompra == 0) 
+            {
+                Console.WriteLine("Venda Inválida!");
+            }
+            else 
+            {
+                Console.WriteLine("Não foi possivel realizar a compra");
+            }
+        }
+
+        public void ApredendoSwitchCase()
+        {
+            Console.WriteLine("Digite uma letra: ");
+            string letra = Console.ReadLine();
+
+            switch (letra)
+            {
+                case "a":
+                Console.WriteLine("É uma vogal.");
+                break;
+                case "e":
+                Console.WriteLine("É uma vogal.");
+                break;
+                case "i":
+                Console.WriteLine("É uma vogal.");
+                break;
+                case "o":
+                Console.WriteLine("É uma vogal.");
+                break;
+                case "u":
+                Console.WriteLine("É uma vogal.");
+                break;
+                default:
+                Console.WriteLine("Não é uma vogal.");
+                break;                
+            }
+        }
     }
 }
