4cf7027874a15ba4714c1b69f47b7c4ae7d1a25f
erica-as <erica.asnt@hotmail.com>
2022-11-29 21:17:28 -0300

    trabalhando com arrays e listas

---
+++ array-listas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace array_listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraysList arrays = new ArraysList();
+
+            arrays.ArrayInteiro();
+            arrays.PercorrerArray();
+            arrays.TrabalhandoComListas();
+        }
+    }
+}
