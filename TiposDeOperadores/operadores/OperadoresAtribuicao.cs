5211c20e63751d15f1a033ffb8ae23a92052f018
erica-as <erica.asnt@hotmail.com>
2022-12-01 21:31:55 -0300

    ajustes namespace TiposDeOperadores

---
+++ TiposDeOperadores/operadores/OperadoresAtribuicao.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiposDeOperadores.operadores
+{
+    public class OperadoresAtribuicao
+    {
+        public void ConverterTiposvariaveis() 
+        {
+
+            //Cast - Casting
+            int i = 12; 
+            double d = 765.12; 
+            float f = 56.123F; 
+
+            //converte um tipo em outro tipo
+            //Convert é uma clasr r To() é um método
+            Console.WriteLine(Convert.ToString(f)); 
+            Console.WriteLine(Convert.ToInt32(d)); 
+            Console.WriteLine(Convert.ToDouble(i));   
+
+            string p = "155";
+
+            Console.WriteLine(Int32.Parse(p));
+
+            //Converte para string
+            int inteiro = 6;
+            string ab = inteiro.ToString();
+            Console.WriteLine(ab);
+
+        }
+
+        public void CastImplicito()
+        {
+            //São tipos diferentes, mas um int cabe em um double e há uma conversão implícitamente
+            int a = 5;
+            double b = a;
+
+            Console.WriteLine(b);
+        }
+
+        public void ConversaoSegura() 
+        {
+            string a = "15";
+            int b = 0;
+
+            int.TryParse(a, out b);
+
+            Console.WriteLine(b);
+            Console.WriteLine("Conversao realizada com sucesso!");
+
+        }
+    }
+}
