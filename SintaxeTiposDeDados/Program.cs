0a7e16df5dd9443fb20202a250846efe5c3fd287
erica-as <erica.asnt@hotmail.com>
2022-12-01 21:29:37 -0300

    ajustes namespace SintaxeTiposDeDados

---
+++ SintaxeTiposDeDados/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SintaxeTiposDeDados.Models;
+
+namespace SintaxeTiposDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoaFisica = new Pessoa();
+            
+            pessoaFisica.Nome = "Érica";
+            pessoaFisica.Idade = 21;
+
+            pessoaFisica.Apresentacao();
+
+            //Date Time é a principal váriavel para se trbalhar com datas 
+            DateTime dataAtual = DateTime.Now.AddDays(5);
+            Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH/mm"));
+
+        }
+    }
+}
