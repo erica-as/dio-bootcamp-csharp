ce6013b0d733921d38448216df9d065770104cac
erica-as <erica.asnt@hotmail.com>
2022-12-01 21:21:28 -0300

    ajuste namespace ArrayLista

---
+++ ArrayListas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraysList arrays = new ArraysList();
+
+            arrays.ArrayInteiro();
+            arrays.PercorrerArray();
+            arrays.TrabalhandoComListas();
+        }
+    }
+}
