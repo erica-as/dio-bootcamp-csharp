7409ee2fc4274aec95220ebf08ef46e814cc3c55
erica-as <erica.asnt@hotmail.com>
2022-12-01 21:26:04 -0300

    ajuste namespace OperadoresAritmeticos

---
+++ OperadoresAritmeticos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using operadores_aritmeticos.calculadora;
+using OperadoresAritmeticos.incremetoDecremento;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadoraBasica = new Calculadora();
+
+            Console.WriteLine("CALCULADORA ");
+            calculadoraBasica.Somar(5, 10);
+            calculadoraBasica.Subtrair(10, 5);
+            calculadoraBasica.Multiplicar(3, 2);
+            calculadoraBasica.Dividir(50, 10);
+            calculadoraBasica.Potencia(3, 3);
+            calculadoraBasica.Seno(30);
+            calculadoraBasica.Coseno(30);
+            calculadoraBasica.Tangente(30);
+            calculadoraBasica.RaizQuadrada(9);
+
+            IncrementoDecremento adicionando = new IncrementoDecremento();
+
+            Console.WriteLine("INCREMENTO E DECREMENTO");
+            adicionando.Incremento();
+            adicionando.Decremento();
+        }
+    }
+}
---
+++ OperadoresAritmeticos/calculadora/Calculadora.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperadoresAritmeticos.calculadora
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            //Utilizamos a classe Math e acessamos o método Pow que realiza a potencia de um número.
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            //Utilizamos a classe Math e acessamos o método Sin que realiza o calculo do radiono do Seno.
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 4)}.");
+        }
+
+        public void Coseno(double angulo)
+        {
+            //Utilizamos a classe Math e acessamos o método Sin que realiza o calculo do radiono do Coseno.
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} = {Math.Round(coseno, 4)}.");
+        }
+
+        public void Tangente(double angulo)
+        {
+            //Utilizamos a classe Math e acessamos o método Sin que realiza o calculo do radiono do Tangente.
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo} = {Math.Round(tangente, 2)}.");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            //Utilizamos a classe Math e acessamos o método Sqrt que realiza o calculo da raiz quadrada.
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz de {x} é {raiz}");
+        }
+    }
+}
